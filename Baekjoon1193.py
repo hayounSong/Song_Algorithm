f5e2af6d1a751e34f6ef7f566bfa8202e3aa4574
HayounSong <fishingest@kookmin.ac.kr>
2021-12-22 01:28:33 +0900

    Error Fix

--- Baekjoon1193.py
+++ Baekjoon1193.py
@@ -14,12 +14,14 @@ while True:
             break;
         else:
             Min= Min + Count
+            Max=Max+Count+1
             Count = Count + 1
 if InputNum==1:
     print("1/1")
 else:
     index=1
     for i in range(Min, Max + 1):
+
         if Count%2==0:
 
             if(InputNum==i):
@@ -31,5 +33,3 @@ else:
                 print(str(Count + 1 - index) + "/" + str(index))
             else:
                 index = index + 1
-
-
