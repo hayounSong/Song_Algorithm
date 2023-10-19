d2f23e9d784cadf49ea3224fa19e03c97c9ec26e
hayounSong <39684920+hayounSong@users.noreply.github.com>
2023-10-19 12:32:43 +0900

    [Silver III] Title: 파도반 수열, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+int main()
+{
+    unsigned long long P[101];
+    P[0] = 0;
+    P[1] = 1;
+    P[2] = 1;
+    P[3] = 1;
+    for (int i = 4; i < 101; i++)
+    {
+        P[i] = P[i - 2] + P[i - 3];
+    }
+
+    int n;
+    cin >> n;
+    vector<int> ans;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        cout << P[a] << endl;
+    }
+}
