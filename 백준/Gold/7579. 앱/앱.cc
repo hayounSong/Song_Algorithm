f0f48633f442bed8692a3e170706ab5d4548c4a9
hayoonSong <39684920+hayounSong@users.noreply.github.com>
2025-01-12 23:55:20 +0900

    [Gold III] Title: 앱, Time: 4 ms, Memory: 13860 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -1,58 +1,42 @@
 #include <iostream>
 using namespace std;
-
-int num[101][10001] = {0};
 int N, M;
-int bytel[10000001];
-int cost[10000001];
+long dp[10001][101] = {0};
+long memory[101] = {0};
+long cost[101] = {0};
+int check[10001][101] = {0};
 int main()
 {
-    cin >> N;
-    cin >> M;
-
+    cin >> N >> M;
+    int sum = 0;
     for (int i = 1; i <= N; i++)
     {
-        cin >> bytel[i];
+        cin >> memory[i];
     }
     for (int i = 1; i <= N; i++)
     {
         cin >> cost[i];
+        sum = sum + cost[i];
     }
-    int amin = 999999;
 
-    for (int i = 1; i <= 100; i++)
+    for (int i = 0; i <= sum; i++)
     {
-
-        for (int j = 1; j <= 10000; j++)
+        dp[i][0] = 0;
+        for (int j = 1; j <= N; j++)
         {
-
-            if (cost[i] > j)
+            if (i >= cost[j])
             {
-                num[i][j] = num[i - 1][j];
+                dp[i][j] = max(dp[i][j - 1], dp[i - cost[j]][j - 1] + memory[j]);
             }
             else
             {
-
-                num[i][j] = max(bytel[i] + num[i - 1][j - cost[i]], num[i - 1][j]);
-
-                // cout << bytel[i] + num[i - 1][j - cost[i]];
-                if (num[i][j] >= M)
-                {
-
-                    if (j < amin)
-                    {
-                        if (j == 4)
-                        {
-                            // cout << "s" << i << " " << j << endl;
-
-                            // cout << num[i - 1][j - cost[i]];
-                        }
-                        amin = j;
-                    }
-                }
+                dp[i][j] = dp[i][j - 1];
+            }
+            if (dp[i][j] >= M)
+            {
+                cout << i;
+                return 0;
             }
         }
     }
-
-    cout << amin;
 }
